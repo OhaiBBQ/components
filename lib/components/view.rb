cca6d67baa9c3c911520ca85f5a96328aa723810
Dan Hansen <dan@backerkit.com>
2014-12-31 14:22:20 -0800

    enable url helpers in component views

--- lib/components/view.rb
+++ lib/components/view.rb
@@ -1,13 +1,30 @@
+require 'ostruct' 
+
 class Components::View
-  attr_reader :template, :assigns, :view
+  attr_reader :assigns, :view
   
   def initialize(options = {})
-    @view = ActionView::Base.new('app/views', {}, ActionController::Base.new)
+    @view = ComponentView.new('app/views', {}, ActionController::Base.new)
     
     @assigns = options.fetch(:assigns, {})
   end
   
   def render(template, locals)
     view.render(partial: template, locals: assigns.merge(locals))
+  end
+
+  # HACK
+  class ComponentView < ActionView::Base
+    include Rails.application.routes.url_helpers
+
+    def optimize_routes_generation?
+      true
+    end
+
+    def url_options
+      {
+        host: 'www.example.com'
+      }
+    end
   end
 end
